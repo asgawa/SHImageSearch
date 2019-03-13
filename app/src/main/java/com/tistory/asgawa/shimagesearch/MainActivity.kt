e368b47c1a2c92a5e08eca70a9507b61ebca3a05
Shinhoon Kang <asgawa@gmail.com>
2019-03-14 01:45:17 +0900

    #BL1 Implement basic UI and user interaction Add SHLog

--- app/src/main/java/com/tistory/asgawa/shimagesearch/MainActivity.kt
+++ app/src/main/java/com/tistory/asgawa/shimagesearch/MainActivity.kt
@@ -1,19 +1,75 @@
 package com.tistory.asgawa.shimagesearch
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
+import android.os.Handler
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
+
+import kotlinx.android.synthetic.main.activity_main.*;
+import android.view.inputmethod.InputMethodManager
+
 
 /**
- * Observe user input $editTextUserInput and search when it does not change for 1 second
- * Search result will be shown in $imageViewSearchResult
- * If an error occurred or there are no result, show UI to user
- * Progress UI is required for long time works
- */
+ * # Business Logic
+ * 1. Observe user input $editTextUserInput and search when it does not change for 1 second
+ * 2. Search when it has triggered
+ * 3. Search result will be shown in $recyclerViewImageResults
+ * 4. If an error occurred or there are no result, show UI to user
+ * 5. Progress UI is required for long time works
+*/
 
 class MainActivity : AppCompatActivity() {
+
+    private val SEARCH_TRIGGER_TIMEOUT = 1000L;
+    private var log: SHLog = SHLog("MainActivity")
+    private var isSearch: Boolean = false
+    private var searchTimeTrigger: Handler = Handler()
+    private val searchTrigger: Runnable = object : Runnable {
+        override fun run() {
+            isSearch = true
+            log.d("1 sec passed")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //#BL1
+        editTextUserInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) { }
+            override fun beforeTextChanged(text: CharSequence?, start: Int, before: Int, after: Int) { }
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, after: Int) {
+                searchTimeTrigger.removeCallbacks(searchTrigger)
+                searchTimeTrigger.postDelayed(searchTrigger, SEARCH_TRIGGER_TIMEOUT)
+            }
+        })
+        editTextUserInput.setOnEditorActionListener { v, actionId, event ->
+            when(actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    hideInputMethod()
+                    true
+                }
+                else -> false
+            }
+        }
+        recyclerViewImageResults.setOnTouchListener { v, event ->
+            hideInputMethod()
+            false
+        }
+
+        //#BL2
+
+        editTextUserInput.requestFocus()
+    }
+
+    private fun hideInputMethod() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (imm.isActive) {
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
---
+++ app/src/main/java/com/tistory/asgawa/shimagesearch/SHLog.java
@@ -0,0 +1,84 @@
+package com.tistory.asgawa.shimagesearch;
+
+import android.util.Log;
+
+/**
+ name: Debug
+ type: class
+ function: offer improved logcat debugging system
+ */
+
+public class SHLog {
+    private String mTag = "SHLog";
+
+    private SHLog() {
+    }
+
+    public SHLog(String tag) {
+        if ((tag != null) && (!tag.isEmpty())) {
+            mTag = tag;
+        }
+    }
+
+    public void in() {
+        StackTraceElement[] stack = new Throwable().getStackTrace();
+        if (stack != null && stack.length > 1) {
+            d("[%s(%s:%d)] IN", stack[1].getClassName(), stack[1].getMethodName(), stack[1].getLineNumber());
+        }
+    }
+
+    public void out() {
+        StackTraceElement[] stack = new Throwable().getStackTrace();
+        if (stack != null && stack.length > 1) {
+            d("[%s(%s:%d)] OUT", stack[1].getClassName(), stack[1].getMethodName(), stack[1].getLineNumber());
+        }
+    }
+
+    public void v(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.v(mTag, str);
+        }
+    }
+
+    public void d(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.d(mTag, str);
+        }
+    }
+
+    public void i(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.i(mTag, str);
+        }
+    }
+
+    public void w(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.w(mTag, str);
+        }
+    }
+
+    public void e(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.e(mTag, str);
+        }
+    }
+
+    public void a(String msg, Object ... args) {
+        if (msg != null && msg.length() > 0) {
+            String str = null;
+            str = String.format(msg, args);
+            Log.wtf(mTag, str);
+        }
+    }
+}
